c6ab143083cdf8802264be0847f86b0838623bfe
wsl3 <2350622075@qq.com>
2019-03-04 20:46:15 +0800

    跨平台

---
+++ repl_2.c
@@ -0,0 +1,50 @@
+/*
+   考虑跨平台
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define _WIN32
+#ifdef _WIN32
+
+#include<string.h>
+#define MAXINPUT 2048
+static char input[MAXINPUT];
+
+char* readline(const char* promt)
+{
+    fputs(promt, stdout); // 不用puts(), 因为还会输出\n
+    fgets(input, MAXINPUT, stdin);
+    
+    char* temp = malloc(strlen(input)+1);
+    
+    strcpy(temp, input);
+    temp[strlen(input)] = '\0';
+    return temp;
+}
+
+void add_history(const char* input){}
+
+#else
+#include<readline/readline.h>
+#include<readline/history.h>
+#endif
+
+int main(int argc, char* argv[])
+{
+    char* input;
+
+    while(1){
+	
+	input = readline("Lispy>>>"); //读入是可以按左右方向键
+
+	//添加到history, 可以按上下键
+	add_history(input);
+
+	printf("your input: %s\n", input);
+	// 必须free()
+	free(input);
+    }
+    return 0;
+}
