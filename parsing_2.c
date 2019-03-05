2bde503bd23f2714ece72ae390ea84e71c27d33d
wsl3 <2350622075@qq.com>
2019-03-05 20:44:15 +0800

    增加小数匹配

--- parsing_2.c
+++ parsing_2.c
@@ -22,7 +22,7 @@ int main(int argc, char* argv[])
 
     mpca_lang(MPCA_LANG_DEFAULT,
 	"                                                     	\
-	number   : /-?[0-9]+/ |'('/-?[0-9]+/')';   \
+	number   : /-?[0-9]+[.]?[0-9]*/ |'('/-?[0-9]+/')';   \       
 	operator : '+' | '-' | '*' | '/' | '%' | \"add\" | \"div\" | \"sub\" | \"mul\" ;   \
 	expr     : <number> | '(' <operator> <expr>+ ')' ;  \
 	lispy    : /^/ <operator> <expr>+ /$/ ;             \
